825b2da88befe60283348ebb896590596542ea9a
Chris-Paish <chris.brown29@auspost.com.au>
2025-04-04 16:30:42 +1100

    Crowbar in AP card

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,18 +1,46 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
-}
+/*! [BUILD VERSION] - 25.2.0 - Tue Mar 11 2025 06:45:48 GMT+0000 (Coordinated Universal Time) */
+!function() {
+  function t(t) {
+      t.replaceWith('<div class="'.concat(t.attr("class"), '" id="').concat(t.attr("id"), '">').concat(t.html(), "</div>"))
+  }
+  $(document).ready((function() {
+      $(".card .tile--simulate-links").each((function(i, n) {
+          var e, a, c = $(n), r = c.find(".tile-title-link"), l = function(t) {
+              if (t.attr("id"))
+                  return t.attr("id");
+              return window.cardTitleCounter = void 0 !== window.cardTitleCounter ? window.cardTitleCounter + 1 : 0,
+              "card-title-id-".concat(window.cardTitleCounter, "-").concat(Math.floor(1e6 * Math.random()))
+          }(c.find(".tile-title")), o = c.find(".tile-image-link"), d = c.find(".cta-wrapper a"), f = [];
+          r.length > 0 && (e = r.attr("href")),
+          o.length > 0 && (a = o.attr("href")),
+          d.length > 0 && d.each((function(t, i) {
+              f.push($(i).attr("href"))
+          }
+          )),
+          o.length > 0 && (void 0 !== e || f.length > 0) && a === e && -1 !== f.indexOf(a) && t(o),
+          r.length > 0 && f.length > 0 && -1 !== f.indexOf(e) && t(r),
+          setTimeout((function() {
+              d.each((function(t, i) {
+                  var n = $(i)
+                    , a = c.find(".tile-title-link, .tile-image-link")
+                    , r = n.attr("href")
+                    , o = n.attr("id") || "anchor-".concat(t, "-").concat(l)
+                    , d = c.find(".tile-title");
+                  d.length > 0 && (d.attr("id", l),
+                  n.attr("id", o),
+                  n.attr("aria-labelledby", "".concat(o, " ").concat(l))),
+                  e === r && (a.css("cursor", "pointer"),
+                  a.on("click", (function() {
+                      n.get(0).click()
+                  }
+                  )))
+              }
+              ))
+          }
+          ))
+      }
+      ))
+  }
+  ))
+}();
+//# sourceMappingURL=bundle.min.js.map
